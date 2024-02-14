b6cd1f911d97fd3e00b13d9dcc19d61a5ff2f297
asier <asier.oyanguren@gmail.com>
2024-02-14 10:47:00 +0100

    FEAT: Camera function implemented
    Camera photos are saved in app storage and not in gallery yet

--- app/src/main/java/com/asier/nemergenttest/MainActivity.java
+++ app/src/main/java/com/asier/nemergenttest/MainActivity.java
@@ -1,18 +1,95 @@
 package com.asier.nemergenttest;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import com.asier.nemergenttest.databinding.ActivityMainBinding;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
+    private ActivityResultLauncher<Intent> cameraLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        createCameraLauncher();
+
+        mBinding.pictureButton.setOnClickListener(view -> {
+            takePicture();
+        });
     }
+
+    private void createCameraLauncher() {
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+
+                            Intent data = result.getData();
+                            Bundle extras = data.getExtras();
+                            File file = null;
+                            try {
+                                file = createImageFile();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            FileOutputStream out = null;
+                            try {
+                                out = new FileOutputStream(file);
+                                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                                out.flush();
+                                out.close();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                });
+    }
+
+
+    private void takePicture() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(cameraIntent);
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", new Locale("en")).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+    }
+
 }
