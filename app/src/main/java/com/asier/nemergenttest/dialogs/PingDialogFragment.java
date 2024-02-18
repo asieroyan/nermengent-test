b8478243384037f6d4266fa6de85b8fc0adb3645
asier <asier.oyanguren@gmail.com>
2024-02-18 02:54:32 +0100

    FEAT: Ping button start and resume function added
    When stopped or finished, dialog shows the current state of the realised pings and waits for more input.

--- app/src/main/java/com/asier/nemergenttest/dialogs/PingDialogFragment.java
+++ app/src/main/java/com/asier/nemergenttest/dialogs/PingDialogFragment.java
@@ -1,19 +1,15 @@
 package com.asier.nemergenttest.dialogs;
 
-import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.Observer;
-import androidx.work.Data;
-import androidx.work.ListenableWorker;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
@@ -25,74 +21,70 @@ import com.asier.nemergenttest.ws.PingGoogleWS;
 public class PingDialogFragment extends DialogFragment {
 
     private FragmentDialogPingBinding mBinding;
-    private Boolean isLoopActive = false;
-    private Integer totalSuccess = 0;
-    private Integer totalFailures = 0;
-    private Integer numTries;
+    private Boolean isStopped = false;
+
+    private SharedPreferences sharedPrefs;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = FragmentDialogPingBinding.inflate(getLayoutInflater());
+        sharedPrefs = requireContext().getSharedPreferences("com.asier.nemergenttest", Context.MODE_PRIVATE);
+
+
         mBinding.startButton.setOnClickListener(view -> {
-            Log.d("dev-FLAG", "START BUTTON ON CLICK LISTENER");
-            this.startLoop(
-                    Integer.parseInt(mBinding.numTriesEditText.getText().toString())
-            );
+            if (mBinding.numTriesEditText.getText().toString().equals("")) {
+                Toast.makeText(requireContext(), R.string.not_value_error, Toast.LENGTH_SHORT).show();
+            } else {
+                if (!isStopped) {
+                    SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+                    prefsEditor.putInt("triesLeft", Integer.parseInt(String.valueOf(mBinding.numTriesEditText.getText())));
+                    prefsEditor.putInt("totalSuccesses", 0);
+                    prefsEditor.putInt("totalFailures", 0);
+                    prefsEditor.putBoolean("isEnd", false);
+                    prefsEditor.commit();
+                }
+                this.startPings();
+            }
         });
+
         mBinding.stopButton.setOnClickListener(view -> {
-            Log.d("dev-FLAG", "STOP BUTTON ON CLICK LISTENER");
+            isStopped = true;
             this.stopLoop();
         });
-        mBinding.closeButton.setOnClickListener(view -> {
-            this.dismiss();
-        });
+
+        mBinding.closeButton.setOnClickListener(view -> this.dismiss());
+
         return mBinding.getRoot();
     }
 
-    public void startPings(Integer numTries) {
-        Data pingData = new Data.Builder()
-                .putInt("numTries", numTries)
-                .build();
+    public void startPings() {
         OneTimeWorkRequest pingSystemOtwr= new OneTimeWorkRequest.Builder(PingGoogleWS.class)
-                .setInputData(pingData)
                 .addTag("pingTask")
                 .build();
 
         WorkManager.getInstance(requireContext()).getWorkInfoByIdLiveData(pingSystemOtwr.getId())
-                .observe(this, new Observer<WorkInfo>() {
-                    @Override
-                    public void onChanged(WorkInfo workInfo) {
-                        if(workInfo != null && workInfo.getState().isFinished()){
-                            String successes = workInfo.getOutputData().getString("totalSuccesses");
-                            String failures = workInfo.getOutputData().getString("totalFailures");
-                            if (successes != null && failures != null) {
-                                showResults(successes, failures);
-                            }
+                .observe(this, workInfo -> {
+
+                    if(workInfo != null && workInfo.getState().isFinished()){
+                        if (sharedPrefs.getBoolean("isEnd", false)) {
+                            isStopped = false;
                         }
+                        showResults();
                     }
                 });
         WorkManager.getInstance(requireContext()).enqueue(pingSystemOtwr);
 
     }
 
-    public void startLoop(Integer numTries) {
-        if (!isLoopActive) { // Start the loop
-            isLoopActive = true;
-            this.totalFailures = 0;
-            this.totalSuccess = 0;
-            this.numTries = numTries;
-            startPings(numTries);
-        }
-    }
-
     public void stopLoop() {
         WorkManager.getInstance(requireContext()).cancelAllWorkByTag("pingTask");
     }
 
-    public void showResults(String successes, String failures) {
-        String successText = getString(R.string.successes) + ": " + successes;
+    public void showResults() {
+        String successText = getString(R.string.successes) + ": " + sharedPrefs.getInt("totalSuccesses", 0);
         mBinding.SuccessesText.setText(successText);
-        String failuresText = getString(R.string.failures) + ": " + failures;
+        String failuresText = getString(R.string.failures) + ": " + sharedPrefs.getInt("totalFailures", 0);
         mBinding.FailuresText.setText(failuresText);
     }
 
--- app/src/main/java/com/asier/nemergenttest/ws/PingGoogleWS.java
+++ app/src/main/java/com/asier/nemergenttest/ws/PingGoogleWS.java
@@ -1,27 +1,17 @@
 package com.asier.nemergenttest.ws;
 
 import android.content.Context;
-import android.util.Log;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
-import androidx.work.Data;
-import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class PingGoogleWS extends Worker {
-
-    private Integer totalSuccess = 0;
-    private Integer totalFailures = 0;
-    private Integer numTries;
-    private Boolean first = true;
+    private final SharedPreferences sharedPrefs =  getApplicationContext().getSharedPreferences("com.asier.nemergenttest", Context.MODE_PRIVATE);
 
     public PingGoogleWS(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -30,52 +20,55 @@ public class PingGoogleWS extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        Log.d("dev-WORK STARTED", "yes");
-        if (first) {
-            first = false;
-            numTries = getInputData().getInt("numTries", 0);
-        }
         String endpoint = "https://www.google.com";
-        HttpURLConnection urlConnection = null;
+        HttpURLConnection urlConnection;
         try {
             URL url = new URL(endpoint);
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setConnectTimeout(1000);
             urlConnection.setRequestMethod("GET");
             int statusCode = urlConnection.getResponseCode();
-            Log.d("dev-STATUS CODE", String.valueOf(statusCode));
-            if (statusCode == 200) {
-
-                totalSuccess++;
-            } else {
-                totalFailures++;
+            if (isStopped()) {
+                return Result.success();
             }
-            Log.d("dev-NUM TRIES 2", numTries.toString());
-            Log.d("dev-TOTAL SUCCESSES", totalSuccess.toString());
-            Log.d("dev-TOTAL FAILURES", totalFailures.toString());
-            if (numTries > 1) {
-                numTries--;
+            if (statusCode == 200) {
+                int tempSuccess = sharedPrefs.getInt("totalSuccesses", 0);
+                tempSuccess++;
+                SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+
+                prefsEditor.putInt("totalSuccesses", tempSuccess);
+
+                prefsEditor.commit();
+            } else {
+                int tempFailures = sharedPrefs.getInt("totalFailures", 0);
+                tempFailures++;
+                SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+
+                prefsEditor.putInt("totalFailures", tempFailures);
+
+                prefsEditor.commit();
+            }
+            if (sharedPrefs.getInt("triesLeft", 0) > 1) {
+                int tempTries = sharedPrefs.getInt("triesLeft", 0);
+                tempTries--;
+                SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+
+                prefsEditor.putInt("triesLeft", tempTries);
+
+                prefsEditor.commit();
                 doWork();
             }
-
-            Log.d("dev-RETURN RESULT", totalSuccess + ", " + totalFailures);
-            Data results = new Data.Builder()
-                    .putString("totalSuccesses", String.valueOf(totalSuccess))
-                    .putString("totalFailures", String.valueOf(totalFailures))
-                    .build();
-            return Result.success(results);
+            setEndState();
+            return Result.success();
         } catch (
                 Exception e) {
             e.printStackTrace();
             return Result.failure();
         }
     }
-
-    @Override
-    public void onStopped() {
-        super.onStopped();
-        first = true;
-        totalSuccess = 0;
-        totalFailures = 0;
+    public void setEndState() {
+        SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+        prefsEditor.putBoolean("isEnd", true);
+        prefsEditor.commit();
     }
 }
